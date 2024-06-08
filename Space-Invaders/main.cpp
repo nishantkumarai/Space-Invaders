d135f809bb5ec8da55df79115c2c3ffb5f1964ca
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2024-06-09 01:36:59 +0530

    Draw some Shapes

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -18,9 +18,32 @@ int main() {
         }
 
         // Clear the window
-        window->clear(sf::Color::Blue);
+        window->clear(sf::Color::White);
 
         // Draw your content here...
+        
+        // Draw a circle
+        sf::CircleShape circle(50); // Radius 50
+        circle.setFillColor(sf::Color::Green);
+        circle.setPosition(400, 300); // Set position
+        window->draw(circle);
+
+        // Draw a square
+        sf::RectangleShape square(sf::Vector2f(100, 100));
+        square.setFillColor(sf::Color::Red); // Red color
+        square.setPosition(300.f, 100.f); // Set position
+        window->draw(square);
+
+        // Draw a tringle
+        sf::ConvexShape triangle;
+        triangle.setPointCount(3);
+        triangle.setPoint(0, sf::Vector2f(500, 100));  // Top point
+        triangle.setPoint(1, sf::Vector2f(650, 250));  // Right point
+        triangle.setPoint(2, sf::Vector2f(350, 250));  // Left point
+        triangle.setFillColor(sf::Color::Blue); // Blue color
+
+        window->draw(triangle);
+
 
         // Display what was drawn
         window->display();
