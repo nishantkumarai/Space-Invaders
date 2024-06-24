37394e9c540194f70a20795a4ea8c42d10d9afd9
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2024-06-25 02:25:48 +0530

    Create Service Locator

---
+++ Space-Invaders/Source/ServiceLocator.cpp
@@ -0,0 +1,36 @@
+#include "ServiceLocator.h"
+
+
+ServiceLocator::ServiceLocator() {
+
+}
+
+ServiceLocator::~ServiceLocator() {
+
+	//destructor
+
+}
+
+void ServiceLocator::createServices() {
+
+}
+
+void ServiceLocator::clearAllServices() {
+
+}
+
+static ServiceLocator::getInstance() {
+
+}
+
+void ServiceLocator::initialize() {
+
+}
+
+void ServiceLocator::update() {
+
+}
+
+void ServiceLocator::render() {
+
+}
