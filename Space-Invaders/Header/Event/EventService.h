270363f8193c2eb47893d54e4ae7178c3fec2266
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2024-07-08 05:00:37 +0530

    Manage Button State

--- Space-Invaders/Header/Event/EventService.h
+++ Space-Invaders/Header/Event/EventService.h
@@ -4,17 +4,32 @@
 
 namespace Event
 {
+	enum class ButtonState
+	{
+		PRESSED,
+		HELD,
+		RELEASED,
+	};
+
 	class EventService
 	{
 	private:
 		sf::Event game_event; //event var
 		sf::RenderWindow* game_window; //ptr to our game window
 
+		ButtonState left_mouse_button_state;
+		ButtonState right_mouse_button_state;
+		ButtonState left_arrow_button_state;
+		ButtonState right_arrow_button_state;
+		ButtonState A_button_state;
+		ButtonState D_button_state;
+
 		bool isGameWindowOpen();
 		bool gameWindowWasClosed(); //for the condition we already had - the title bar cross.
 		bool hasQuitGame(); //for our new 'ESC' condition
 		bool isKeyboardEvent();
-		
+		void updateMouseButtonsState(ButtonState& current_button_state, sf::Mouse::Button mouse_button);
+		void updateKeyboardButtonsState(ButtonState& current_button_state, sf::Keyboard::Key keyboard_button);
 
 
 	public:
@@ -29,7 +44,8 @@ namespace Event
 		bool pressedEscapeKey();
 		bool pressedLeftKey();
 		bool pressedRightKey();
-
+		bool pressedAKey();
+		bool pressedDKey();
 		bool pressedLeftMouseButton();
 		bool pressedRightMouseButton();
 	};
--- Space-Invaders/Source/Event/EventService.cpp
+++ Space-Invaders/Source/Event/EventService.cpp
@@ -21,7 +21,12 @@ namespace Event
 
     void EventService::update()
     {
-        //for later
+        updateMouseButtonsState(left_mouse_button_state, sf::Mouse::Left);
+        updateMouseButtonsState(right_mouse_button_state, sf::Mouse::Right);
+        updateKeyboardButtonsState(left_arrow_button_state, sf::Keyboard::Left);
+        updateKeyboardButtonsState(right_arrow_button_state, sf::Keyboard::Right);
+        updateKeyboardButtonsState(A_button_state, sf::Keyboard::A);
+        updateKeyboardButtonsState(D_button_state, sf::Keyboard::D);
     }
 
     void EventService::processEvents()
@@ -43,6 +48,20 @@ namespace Event
     } // only true if the ESC key is pressed and a keyboard event has been registered
 
     //checks for if a keyboard key has been pressed
+    bool EventService::pressedLeftKey() { return left_arrow_button_state == ButtonState::HELD; }
+
+    bool EventService::pressedRightKey() { return right_arrow_button_state == ButtonState::HELD; }
+
+    bool EventService::pressedAKey() { return A_button_state == ButtonState::HELD; }
+
+    bool EventService::pressedDKey() { return D_button_state == ButtonState::HELD; }
+
+    bool EventService::pressedLeftMouseButton() { return left_mouse_button_state == ButtonState::PRESSED; }
+
+    bool EventService::pressedRightMouseButton() { return right_mouse_button_state == ButtonState::PRESSED; }
+
+
+
     bool EventService::isKeyboardEvent() { return game_event.type == sf::Event::KeyPressed; }
 
     //control click on the SFML functions to see what they do internally
@@ -52,21 +71,49 @@ namespace Event
 
     bool EventService::gameWindowWasClosed() { return game_event.type == sf::Event::Closed; }
 
-    bool EventService::pressedLeftKey() { return game_event.key.code == sf::Keyboard::Left; }
-    bool EventService::pressedRightKey() { return game_event.key.code == sf::Keyboard::Right; }
+ 
 
-    bool EventService::pressedLeftMouseButton()
+    void EventService::updateMouseButtonsState(ButtonState& current_button_state, sf::Mouse::Button mouse_button)
     {
-        // check if a mouse button was pressed and which mouse button it was
-        return game_event.type == sf::Event::MouseButtonPressed && game_event.mouseButton.button == sf::Mouse::Left;
+        if (sf::Mouse::isButtonPressed(mouse_button))
+        {
+            switch (current_button_state)
+            {
+            case ButtonState::RELEASED:
+                current_button_state = ButtonState::PRESSED;
+                break;
+            case ButtonState::PRESSED:
+                current_button_state = ButtonState::HELD;
+                break;
+            }
+        }
+        else
+        {
+            current_button_state = ButtonState::RELEASED;
+        }
     }
 
-    bool EventService::pressedRightMouseButton()
+    void EventService::updateKeyboardButtonsState(ButtonState& current_button_state, sf::Keyboard::Key keyboard_button)
     {
-        /*
-        // same as above for the right button, if we want to we can move the mouse button
-        // press check to another function altogether.
-        */
-        return game_event.type == sf::Event::MouseButtonPressed && game_event.mouseButton.button == sf::Mouse::Right;
+        if (sf::Keyboard::isKeyPressed(keyboard_button))
+        {
+            switch (current_button_state)
+            {
+            case ButtonState::RELEASED:
+                current_button_state = ButtonState::PRESSED;
+                break;
+            case ButtonState::PRESSED:
+                current_button_state = ButtonState::HELD;
+                break;
+            }
+        }
+        else
+        {
+            current_button_state = ButtonState::RELEASED;
+        }
     }
+
+
+   
+
 }
