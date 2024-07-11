9ea6fea46800ef0026a028c23ad971f26edaaed6
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2024-07-11 05:44:04 +0530

    Implement Enemy Movement

--- Space-Invaders/Source/Enemy/EnemyModel.cpp
+++ Space-Invaders/Source/Enemy/EnemyModel.cpp
@@ -8,6 +8,7 @@ namespace Enemy
 
 	void EnemyModel::initialize()
 	{
+		movement_direction = MovementDirection::RIGHT;
 		enemy_position = reference_position;
 	}
 
@@ -29,6 +30,16 @@ namespace Enemy
 	void EnemyModel::setReferencePosition(sf::Vector2f position)
 	{
 		reference_position = position;
+	}
+
+	MovementDirection EnemyModel::getMovementDirection()
+	{
+		return movement_direction;
+	}
+
+	void EnemyModel::setMovementDirection(MovementDirection direction)
+	{
+		movement_direction = direction;
 	}
 
 }
