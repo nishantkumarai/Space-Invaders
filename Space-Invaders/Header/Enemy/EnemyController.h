7ffe218c1825eec7f50298ddccb9fcd7b7cc5b05
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2024-07-19 02:06:03 +0530

    Create Common Enemy Properties

--- Space-Invaders/Header/Enemy/EnemyController.h
+++ Space-Invaders/Header/Enemy/EnemyController.h
@@ -6,34 +6,31 @@ namespace Enemy
     class EnemyView;
     class EnemyModel;
 
+    enum class EnemyType;
     enum class EnemyState;
+
 
     class EnemyController
     {
     private:
-        const sf::Vector2f left_most_position = sf::Vector2f(50.f, 950.f);
-        const sf::Vector2f right_most_position = sf::Vector2f(1800.f, 950.f);
-
-        const float vertical_travel_distance = 100.f;
-        const float enemy_movement_speed = 250.0f;
-
+       
         EnemyView* enemy_view;
         EnemyModel* enemy_model;
 
-        void move();
+        virtual void move();
         void moveLeft();
         void moveRight();
         void moveDown();
 
     public:
         EnemyController();
-        ~EnemyController();
+        virtual ~EnemyController();
 
-        void initialize();
+        virtual void initialize();
         void update();
         void render();
 
         sf::Vector2f getEnemyPosition();
-        EnemyState getEnemyState();
+        
     };
 }
