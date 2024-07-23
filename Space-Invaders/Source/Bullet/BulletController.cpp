0d9bc647708865162161f35e50582ff3f1cf4602
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2024-07-23 17:29:46 +0530

    Create Bullet MVC

---
+++ Space-Invaders/Source/Bullet/BulletController.cpp
@@ -0,0 +1,52 @@
+#include "../../Header/Bullet/BulletController.h"
+
+namespace Bullet
+{
+	void BulletController::updateProjectilePosition()
+	{
+	}
+
+	void BulletController::moveUp()
+	{
+	}
+
+	void BulletController::moveDown()
+	{
+	}
+
+	void BulletController::handleOutOfBounds()
+	{
+	}
+
+	BulletController::BulletController(BulletType type)
+	{
+	}
+
+	BulletController::~BulletController()
+	{
+	}
+
+	void BulletController::initialize(sf::Vector2f position, Bullet::MovementDirection direction)
+	{
+	}
+
+	void BulletController::update()
+	{
+	}
+
+	void BulletController::render()
+	{
+	}
+
+	sf::Vector2f BulletController::getProjectilePosition()
+	{
+		return sf::Vector2f();
+	}
+
+	BulletType BulletController::getBulletType()
+	{
+		return BulletType();
+	}
+
+
+}
