6946549b99f3fafbf79fa4fc2d3d66743156801d
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2024-07-26 12:32:47 +0530

    Create the Powerup Controllers

---
+++ Space-Invaders/Source/Powerup/PowerupService.cpp
@@ -0,0 +1,34 @@
+#include "../../Header/Powerups/PowerupService.h"
+
+namespace Powerup
+{
+	PowerupController* PowerupService::createPowerup(PowerupType powerup_type)
+	{
+		return nullptr;
+	}
+	void PowerupService::destroy()
+	{
+	}
+	PowerupService::PowerupService()
+	{
+	}
+	PowerupService::~PowerupService()
+	{
+	}
+	void PowerupService::initialize()
+	{
+	}
+	void PowerupService::update()
+	{
+	}
+	void PowerupService::render()
+	{
+	}
+	PowerupController* PowerupService::spawnPowerup(PowerupType powerup_type, sf::Vector2f position)
+	{
+		return nullptr;
+	}
+	void PowerupService::destroyPowerup(PowerupController* powerup_controller)
+	{
+	}
+}
